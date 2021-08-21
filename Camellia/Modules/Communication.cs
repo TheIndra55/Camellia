1bc87d54dd5aa3ddf30f63909839df29e11a3df1
Xwilarg <xwilarg@yahoo.fr>
2021-08-21 14:25:08 +0200

    Add JSON and XML commands

--- Camellia/Modules/Communication.cs
+++ Camellia/Modules/Communication.cs
@@ -14,7 +14,9 @@ namespace Camellia.Modules
                 Description =
                     "**Length [text]:** Give the length of a string\n" +
                     "**Hex [decimal number]:** Convert decimal to hexadecimal\n" +
-                    "**Dec [hexadecimal number]:** Convert hexadecimal to decimal\n",
+                    "**Dec [hexadecimal number]:** Convert hexadecimal to decimal\n" +
+                    "**JSON [text]:** Check if a JSON is valid or not\n" +
+                    "**XML [text]:** Check if an XML is valid or not\n",
                 Color = Color.Blue
             }.Build());
         }
--- Camellia/Modules/Science.cs
+++ Camellia/Modules/Science.cs
@@ -1,12 +1,59 @@
 ﻿using Discord;
 using Discord.Commands;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Camellia.Modules
 {
     public class Science : ModuleBase
     {
+        [Command("Json")]
+        public async Task JsonAsync([Remainder]string str)
+        {
+            if (str.StartsWith("```json") && str.EndsWith("```"))
+            {
+                str = str[7..^3];
+            }
+            else if (str.StartsWith("```") && str.EndsWith("```"))
+            {
+                str = str[3..^3];
+            }
+            try
+            {
+                JsonConvert.DeserializeObject(str);
+                await ReplyAsync("Your JSON is valid");
+            }
+            catch (JsonReaderException e)
+            {
+                await ReplyAsync("Your JSON is **not** valid:\n```" + e.Message + "\n```");
+            }
+        }
+
+        [Command("XML")]
+        public async Task XmlAsync([Remainder] string str)
+        {
+            if (str.StartsWith("```xml") && str.EndsWith("```"))
+            {
+                str = str[6..^3];
+            }
+            else if (str.StartsWith("```") && str.EndsWith("```"))
+            {
+                str = str[3..^3];
+            }
+            var xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.LoadXml(str);
+                await ReplyAsync("Your XML is valid");
+            }
+            catch (XmlException e)
+            {
+                await ReplyAsync("Your XML is **not** valid:\n```" + e.Message + "\n```");
+            }
+        }
+
         [Command("Length")]
         public async Task Length([Remainder]string str)
         {
@@ -30,13 +77,13 @@ namespace Camellia.Modules
         }
 
         [Command("Hex")]
-        public async Task Hex(params int[] numbers)
+        public async Task HexAsync(params int[] numbers)
         {
             await ReplyAsync(string.Join(" ", numbers.Select(x => x.ToString("X"))));
         }
 
         [Command("Dec")]
-        public async Task Dec(params Hex[] numbers)
+        public async Task DecAsync(params Hex[] numbers)
         {
             await ReplyAsync(string.Join(" ", numbers.Select(x => x.Value)));
         }
