40301b74591fe62013627ba1f156b964de97233d
Christian Chaux <xwilarg@yahoo.fr>
2021-09-13 12:39:14 +0200

    Add calc to help

--- Camellia/Modules/Communication.cs
+++ Camellia/Modules/Communication.cs
@@ -16,7 +16,8 @@ namespace Camellia.Modules
                     "**Hex [decimal number]:** Convert decimal to hexadecimal\n" +
                     "**Dec [hexadecimal number]:** Convert hexadecimal to decimal\n" +
                     "**JSON [text]:** Check if a JSON is valid or not\n" +
-                    "**XML [text]:** Check if an XML is valid or not\n",
+                    "**XML [text]:** Check if an XML is valid or not\n" +
+                    "**Calc [operation]:** Evaluate a mathematical expression"
                 Color = Color.Blue,
                 Footer = new EmbedFooterBuilder
                 {
